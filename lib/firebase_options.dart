240e403061a6036d385a7a101d8991c060d5bab8
sanjay <sanjay13649@gmail.com>
2025-07-18 11:55:28 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,12 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 
 const FirebaseOptions firebaseOptions = FirebaseOptions(
-   apiKey: "AIzaSyAFY_kZPGajTCEC4nCSZF3cHuBWJCn-BTU",
-  appId: "1:539688015669:android:218122f1eab2768975de7c",
-  messagingSenderId: "539688015669",
-  projectId: "pollinai",
-  storageBucket: "pollinai.firebasestorage.app",
-  androidClientId: "539688015669-7eittnuamgdarmiaurbmeaso1u5qloo3.apps.googleusercontent.com"
+you api here
 
   // If you use iOS, add iOS options here as well
 );
